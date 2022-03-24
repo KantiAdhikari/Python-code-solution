c8a1e22e81e03ce1f8e9bc7ed9eef3f3820953a9
Kanti Adhikari <99948702+KantiAdhikari@users.noreply.github.com>
2022-03-24 10:18:31 +0545

    Added the solution for Day 1

---
+++ Day 1/challenges.py
@@ -0,0 +1,32 @@
+#Challenge 1 - Printing statement:
+ 
+print('Day 1- python print function')
+print("The function is declared like this:")
+print("print('What to print')")
+
+
+# Challenge 2 - Character Counting:
+
+print(len(input('What is your name? ')))
+#Or
+
+name=(input('What is your name? '))
+length=len(name)
+print(length)
+
+# Challenge 3 - Interchange the value of variables:
+
+a=input('a:')
+b=input('b:')
+
+c=b
+b=a
+a=c
+
+print('a='+ a)
+print('b='+ b)
+
+
+
+
+
