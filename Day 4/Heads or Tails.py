c51995a02e3dd575a60c5fe17db4b38af6d9389d
Kanti Adhikari <99948702+KantiAdhikari@users.noreply.github.com>
2022-03-24 11:04:17 +0545

    Added the solution for Day 4

---
+++ Day 4/Heads or Tails.py
@@ -0,0 +1,9 @@
+# To check wheather it is Heads or Tails 
+import random
+test_seed=int(input("Create a seed number: "))
+random.seed(test_seed)
+random_side=random.randint(0, 1)
+if random_side==1:
+ print("Heads")
+else:
+ print("Tails")
